ccfbf6fdc42182c8f6576598090408e0c51eef4c
Alex Kozack <kozackunisoft@gmail.com>
2016-12-29 15:58:55 +0200

    Добавлен выбор между типами Шаблона

--- app/routes/keywords/module.js
+++ app/routes/keywords/module.js
@@ -9,6 +9,7 @@ module.exports = angular.module('keywords', [])
 .controller('keywordsCtrl', function ($scope, directService, $state, $mdToast) {
 	if (!directService.getData().length) return $state.go('getDirect');
 	$scope.isLoader = false;
+	$scope.target = 'custom';
 	$scope.keywords = [];
 
 	$scope.parseKeywords = () => {
@@ -18,10 +19,9 @@ module.exports = angular.module('keywords', [])
 		if (!mime.lookup(file) || mime.lookup(file).split('/')[0] != 'text') return $scope.showMess('Данный файл не может быть прочитан');
 		
 		$scope.isLoader = true;
-		// $scope.$apply();
 		openFile(file).then(file_content => {
 			file_content = parseTSV(file_content);
-			if (file_content[1].length == 3) $scope.keywords = parseFromCustom(file_content);
+			if ($scope.target == 'custom') $scope.keywords = parseFromCustom(file_content);
 			else $scope.keywords = parseFromAdwords(file_content);
 			$scope.isLoader = false;
 			$scope.$apply();
@@ -31,7 +31,6 @@ module.exports = angular.module('keywords', [])
 			$scope.showMess( e.toString() || 'Ошибка')
 		})
 	}
-
 
 	$scope.setKeywords = () => {
 		dialog.showMessageBox({
